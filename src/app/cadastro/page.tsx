0fb20eba809220d91db30b563a5393a61f914c80
DevzIcaro <contato-Devicaro@hotmail.com>
2026-04-19 18:35:51 -0300

    shadcn + front de Login + components instalados

---
+++ src/app/cadastro/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "components/login-form";
+
+export default async function Login() {
+    return (
+        <div className="bg-gray-700">
+            <LoginForm />
+        </div>
+    )
+}
